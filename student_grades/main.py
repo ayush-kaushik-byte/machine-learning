Corrected gradient.py

student_grades/main.py
@@ -15,21 +15,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 linear_model = get_model(X_train, y_train)
 
-predictions = linear_model.predict(X_test)
+predictions1 = linear_model.predict(X_test)
 
-print('MAE:', metrics.mean_absolute_error(y_test, predictions))
-print('MSE:', metrics.mean_squared_error(y_test, predictions))
-print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
-print('Accuracy:', linear_model.score(X_test, y_test))
+# print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+# print('MSE:', metrics.mean_squared_error(y_test, predictions))
+# print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+# print('Accuracy:', linear_model.score(X_test, y_test))
 
-# gd = GradientDescent(X_train, y_train, X_test)
+gd = GradientDescent(X_train, y_train, X_test)
 
-# # cost = gd.fit(0.01, 100)
-# gd.fit(0.001, 50)
+gd.fit(0.001, 100)
 
-# predictions2 = gd.predict()
+predictions2 = gd.predict()
 
-# plotter = Plotter()
+plotter = Plotter()
 
-# print(gd.get_theta())
-# print(linear_model.coeff_)
+plotter.scatterplot(predictions2,predictions1)

student_grades/models/gradient.py
@@ -13,6 +13,7 @@ class GradientDescent:
 		self.X_test = np.insert(self.X_test, 0, np.ones((self.X_test.shape[0])), axis=1)
 		self.y = np.array(y).reshape(self.y_shape[0], 1)
 		self.theta = np.zeros((self.x_shape[1], 1))
+		self.m = self.x_shape[0]
 
 	def cost_function(self, X, y, theta):
 
@@ -22,7 +23,7 @@ class GradientDescent:
 	def fit(self, alpha, iterations):
 
 		for i in range(iterations):
-			self.theta -= alpha * (np.dot(self.X.T, (np.dot(self.X, self.theta) - self.y)))
+			self.theta -= alpha * (np.dot(self.X.T, (np.dot(self.X, self.theta) - self.y))) / (self.x_shape[0])
 
 	def predict(self):
 		return np.dot(self.X_test, self.theta)